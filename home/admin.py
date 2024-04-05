29a09af0cd0eb0cd84a6a98a06190fc0f69e0d5f
perfumegoddess <queenjossey@gmail.com>
2024-04-05 11:28:24 +0100

    asdmin and model commit

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Portfolio, About, Skill, Category, Home
 
 # Register your models here.
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ['title', 'link']
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ['id']
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['name', 'percentage']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
--- home/models.py
+++ home/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='portfolio_images/')
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+class About(models.Model):
+    bio = models.TextField()
+    image = models.ImageField(upload_to='about_images/')
+    # Assuming you have some fields for social media links
+    facebook_link = models.URLField(blank=True)
+    twitter_link = models.URLField(blank=True)
+    linkedin_link = models.URLField(blank=True)
+    # Add more social media fields as needed
+
+    def __str__(self):
+        return "About"
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    percentage = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Home(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return "Home"
